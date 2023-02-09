a4da8e09246113b43d56c9624749f9e6e89e0900
light <y.khrystiuk@dev-branch.com>
2023-02-09 23:48:09 +0200

    add range slider

--- src/index.js
+++ src/index.js
@@ -1,8 +1,37 @@
 import './index.html';
 
 import './style.scss';
-import  '../node_modules/vanilla-rangeslider/js/rangeslider.min.js'
+// Import noUiSlider
+import noUiSlider from 'nouislider';
+import range from './img/buttonRangeslider.svg'
 
 
-console.log('test');
 
+var formslider = document.querySelector('.form-slider');
+
+noUiSlider.create(formslider , {
+   start: [70],
+   connect: 'lower',
+    step: 10,
+    range: {
+        'min': 0,
+        'max': 100
+    }
+});
+
+
+formslider.noUiSlider.on('update', function (values, handle) {
+    let stepSliderValueElement = document.querySelector('.form-slider__text'),
+        hidenInput = document.querySelector('.form-slider__child');
+    stepSliderValueElement.innerHTML = (values[handle] / 100);
+    hidenInput.values = stepSliderValueElement.innerHTML;
+});
+
+const createImg = document.createElement("img"),
+      buttonDef = document.querySelector('.noUi-touch-area');
+
+
+
+
+    createImg.src = range;
+    buttonDef.appendChild(createImg);
