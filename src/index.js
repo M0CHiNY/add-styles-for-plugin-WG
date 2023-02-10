3c14c1985dadb9cb4258f9bf165e71d36af9f586
light <y.khrystiuk@dev-branch.com>
2023-02-10 15:47:14 +0200

    correct range-slider js

--- src/index.js
+++ src/index.js
@@ -3,10 +3,11 @@ import './index.html';
 import './style.scss';
 // Import noUiSlider
 import noUiSlider from 'nouislider';
+// Get image in folder img
 import range from './img/buttonRangeslider.svg'
 
 
-
+// init range slider
 var formslider = document.querySelector('.form-slider');
 
 noUiSlider.create(formslider , {
@@ -16,22 +17,24 @@ noUiSlider.create(formslider , {
     range: {
         'min': 0,
         'max': 100
-    }
+    },
 });
 
-
+// function update addEvenListener 
 formslider.noUiSlider.on('update', function (values, handle) {
-    let stepSliderValueElement = document.querySelector('.form-slider__text'),
-        hidenInput = document.querySelector('.form-slider__child');
-    stepSliderValueElement.innerHTML = (values[handle] / 100);
-    hidenInput.values = stepSliderValueElement.innerHTML;
+    let stepSliderValueElement = document.querySelector('.form-slider__text'),//get slider text 
+        hidenInput = document.querySelector('.form-slider__child'); // hiden input 
+    stepSliderValueElement.innerHTML = (values[handle] / 100); 
+    hidenInput.values = stepSliderValueElement.innerHTML; // get value to hiden input
 });
 
-const createImg = document.createElement("img"),
-      buttonDef = document.querySelector('.noUi-touch-area');
 
+const createImg = document.createElement("img"), // create img tag <img>
+      buttonDef = document.querySelector('.noUi-handle'); // search touch 
 
+      function setImage(img, value){ // get tag img and 
+        img.src = value;
+        buttonDef.appendChild(img); // insert image to html
+      }
 
-
-    createImg.src = range;
-    buttonDef.appendChild(createImg);
+ setImage(createImg, range);  //call function setImage ();
