e8d096eea4cd4dcdf8c52952673a9df80f03dfe2
Volodymyr <volodimrmihnyk@gmail.com>
2023-02-15 22:17:33 +0200

    add checkbox

--- src/index.js
+++ src/index.js
@@ -41,4 +41,58 @@ ResetButton.addEventListener('click', ()=> {
 })
 
 
+//Keywords
+
+const ul = document.querySelector(".chat__key-list"),
+  input = document.querySelector(".chat__key"),
+  tagNumb = document.querySelector(".chat__keys");
+
+let maxTags = 10,
+  tags = ['Sport', 'Sport News'];
+  
+
+countTags();
+createTag();
+
+function countTags() {
+  input.focus();
+  tagNumb.innerText = maxTags - tags.length;
+}
+
+function createTag() {
+  ul.querySelectorAll("li").forEach((li) => li.remove());
+  tags
+    .slice()
+    .reverse()
+    .forEach((tag) => {
+      let liTag = `<li>${tag} <i class="uit uit-multiply" onclick="remove(this, '${tag}')"></i></li>`;
+      ul.insertAdjacentHTML("afterbegin", liTag);
+    });
+  countTags();
+}
+
+function remove(element, tag) {
+  let index = tags.indexOf(tag);
+  tags = [...tags.slice(0, index), ...tags.slice(index + 1)];
+  element.parentElement.remove();
+  countTags();
+}
+
+function addTag(e) {
+  if (e.key == "Enter") {
+    let tag = e.target.value.replace(/\s+/g, " ");
+    if (tag.length > 1 && !tags.includes(tag)) {
+      if (tags.length < 10) {
+        tag.split(",").forEach((tag) => {
+          tags.push(tag);
+          createTag();
+        });
+      }
+    }
+    e.target.value = "";
+  }
+}
+
+input.addEventListener("keyup", addTag);
+console.log('reeeeeeeee');
 
