5be574135fd5124b21974158e64d800019018aa3
Volodymyr <volodimrmihnyk@gmail.com>
2023-02-17 21:32:13 +0200

    add fix checkbox

--- src/index.js
+++ src/index.js
@@ -7,48 +7,47 @@ import noUiSlider from 'nouislider';
 
 
 // init range slider
-var formslider = document.querySelector('.form-slider');
-var hidenInput = document.querySelector('.form-slider__child');
+// var formslider = document.querySelector('.form-slider');
+// var hidenInput = document.querySelector('.form-slider__child');
 
-noUiSlider.create(formslider , {
-   start: hidenInput.value,
-   connect: 'lower',
-    step: 10,
-    range: {
-        'min': 0,
-        'max': 100
-    },
-});
+// noUiSlider.create(formslider , {
+//    start: hidenInput.value,
+//    connect: 'lower',
+//     step: 10,
+//     range: {
+//         'min': 0,
+//         'max': 100
+//     },
+// });
 
 
-// function update addEvenListener 
-formslider.noUiSlider.on('update', function (values, handle) {
-    let stepSliderValueElement = document.querySelector('.form-slider__text');//get slider text 
-    // hidenInput = document.querySelector('.form-slider__child'); // hiden input 
-    stepSliderValueElement.innerHTML = (values[handle] / 100); 
-    hidenInput.values = stepSliderValueElement.innerHTML; // get value to hiden input
-});
+// // function update addEvenListener 
+// formslider.noUiSlider.on('update', function (values, handle) {
+//     let stepSliderValueElement = document.querySelector('.form-slider__text');//get slider text 
+//     // hidenInput = document.querySelector('.form-slider__child'); // hiden input 
+//     stepSliderValueElement.innerHTML = (values[handle] / 100); 
+//     hidenInput.values = stepSliderValueElement.innerHTML; // get value to hiden input
+// });
 
-const ResetButton = document.querySelector('.settings__btn-reset');
+// const ResetButton = document.querySelector('.settings__btn-reset');
 
-ResetButton.addEventListener('click', ()=> {
-    const saveButton = document.querySelector('.settings__btn-save');
-    saveButton.classList.add('button-active');// addClass
-    formslider.noUiSlider.set(0);// reset range slider
-    this.addEventListener('click', ()=>{
-        this.classList.remove ('button-active');// removeClass
-    })
-})
+// ResetButton.addEventListener('click', ()=> {
+//     const saveButton = document.querySelector('.settings__btn-save');
+//     saveButton.classList.add('button-active');// addClass
+//     formslider.noUiSlider.set(0);// reset range slider
+//     this.addEventListener('click', ()=>{
+//         this.classList.remove ('button-active');// removeClass
+//     })
+// })
 
 
 //Keywords
 
 const ul = document.querySelector(".chat__key-list"),
   input = document.querySelector(".chat__key"),
-  tagNumb = document.querySelector(".chat__keys");
+  tagNumb = document.querySelector(".chat__details .chat__keys");
 
-let maxTags = 10,
-  tags = ['Sport', 'Sport News'];
+let tags = ['Sport', 'Sport News'];
   
 
 countTags();
@@ -56,7 +55,6 @@ createTag();
 
 function countTags() {
   input.focus();
-  tagNumb.innerText = maxTags - tags.length;
 }
 
 function createTag() {
@@ -94,5 +92,5 @@ function addTag(e) {
 }
 
 input.addEventListener("keyup", addTag);
-console.log('reeeeeeeee');
+
 
